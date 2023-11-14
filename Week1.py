d9531939dbb9d19843827bacdf3accd8ca8e8884
armankarshenas <karshenas@Armans-MacBook-Pro.local>
2023-11-13 22:15:45 -0800

    updates

--- Week1.py
+++ Week1.py
@@ -29,7 +29,37 @@ def FrequentWords(Text,k):
         if freq[key] == m:
             words.append(key)
     return words
+def Reverse_array(Text):
+    rev_text = []
+    for i in range(len(Text)):
+        rev_text.append(Text[len(Text)-1-i])
+    return rev_text
+def Reverse(Text):
+    rev_text = ""
+    for char in Text:
+        rev_text = char + rev_text
+    return rev_text
+def Complement(Text):
+    comp_text = ""
+    comp = {'A':'T','C':'G','T':'A','G':'C'}
+    for char in Text:
+        comp_text = comp_text + comp[char]
+    return comp_text
+def Reverse_complement(Text):
+    rev_comp_text = ""
+    rev_comp_text = Reverse(Text)
+    rev_comp_text = Complement(rev_comp_text)
+    return rev_comp_text
+def PatternMatching(Pattern, Genome):
+    position = []
+    n_p = len(Pattern)
+    n_g = len(Genome)
+    for i in range(n_g-n_p+1):
+        if Genome[i:i+n_p] == Pattern:
+            position.append(i)
+    return position
 ori = "ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
 Pattern = "TGATCA"
 print(PatternCount(ori,Pattern))
+print(Reverse_complement("ATTCG"))
 print(FrequentWords(ori,10))
